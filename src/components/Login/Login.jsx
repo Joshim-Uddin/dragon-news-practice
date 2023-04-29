1f2ff5237555658350c141496a6e520e03410711
Joshim Uddin <mju881@gmail.com>
2023-04-29 06:23:09 +0600

    firebase secured

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -22,33 +22,33 @@ const Login = () => {
       <form onSubmit={handleSubmit} className="login mx-auto my-2 bg-light p-3">
         <h3 className="text-center my-3">Login Your Account</h3>
         <div className="text-left">
-          <div class="mb-3">
-            <label for="exampleInputEmail1" class="form-label">
+          <div className="mb-3">
+            <label htmlFor="exampleInputEmail1" className="form-label">
               Email address
             </label>
             <input
               type="email"
               name="email"
-              class="form-control"
+              className="form-control"
               id="exampleInputEmail1"
               aria-describedby="emailHelp"
             />
           </div>
-          <div class="mb-3">
-            <label for="exampleInputPassword1" class="form-label">
+          <div className="mb-3">
+            <label htmlFor="exampleInputPassword1" className="form-label">
               Password
             </label>
             <input
               type="password"
               name="password"
-              class="form-control"
+              className="form-control"
               id="exampleInputPassword1"
             />
           </div>
         </div>
 
         <div className="text-center">
-          <button type="submit" class="w-100 btn btn-dark">
+          <button type="submit" className="w-100 btn btn-dark">
             Login
           </button>
         </div>
--- src/firebase/firebase.config.js
+++ src/firebase/firebase.config.js
@@ -1,16 +1,13 @@
-// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
 
-// Your web app's Firebase configuration
+
 const firebaseConfig = {
-  apiKey: "AIzaSyBcmGRIV4NYrhiPVX9AXFWPXb7P1JOFF_4",
-  authDomain: "dragon-news-practice-3a37d.firebaseapp.com",
-  projectId: "dragon-news-practice-3a37d",
-  storageBucket: "dragon-news-practice-3a37d.appspot.com",
-  messagingSenderId: "1094645985860",
-  appId: "1:1094645985860:web:e2782d7baa4c2366f70211"
+  apiKey: import.meta.env.VITE_APIKEY,
+  authDomain: import.meta.env.VITE_AUTHDOMAIN,
+  projectId: import.meta.env.VITE_PROJECTID,
+  storageBucket: import.meta.env.VITE_STORAGEBUCKET,
+  messagingSenderId: import.meta.env.VITE_MESSAGINGSENDERID,
+  appId: import.meta.env.VITE_APPID
 };
 
 // Initialize Firebase
